f47b935a920c45a7fd0eea96531e44f5871b93be
Tomasz Setkowski <tom@ai-traders.com>
2015-07-05 14:14:19 +0200

    #1133 added json config plugin settings handling

---
+++ src/com.tw.go.config.json/JSONUtils.java
@@ -0,0 +1,14 @@
+package com.tw.go.config.json;
+
+
+import com.google.gson.GsonBuilder;
+
+public class JSONUtils {
+    public static Object fromJSON(String json) {
+        return new GsonBuilder().create().fromJson(json, Object.class);
+    }
+
+    public static String toJSON(Object object) {
+        return new GsonBuilder().create().toJson(object);
+    }
+}
--- src/com.tw.go.config.json/JsonConfigPlugin.java
+++ src/com.tw.go.config.json/JsonConfigPlugin.java
@@ -12,13 +12,19 @@ import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import com.thoughtworks.go.plugin.api.response.DefaultGoPluginApiResponse;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 import com.thoughtworks.go.plugin.configrepo.messages.ParseDirectoryMessage_1;
+import org.apache.commons.io.IOUtils;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 @Extension
 public class JsonConfigPlugin implements GoPlugin {
+    public static final String PLUGIN_SETTINGS_GET_CONFIGURATION = "go.plugin-settings.get-configuration";
+    public static final String PLUGIN_SETTINGS_GET_VIEW = "go.plugin-settings.get-view";
+    public static final String PLUGIN_SETTINGS_VALIDATE_CONFIGURATION = "go.plugin-settings.validate-configuration";
+    public static final String PLUGIN_SETTINGS_ENVIRONMENT_PATTERN = "environment_pattern";
+
     @Override
     public void initializeGoApplicationAccessor(GoApplicationAccessor goApplicationAccessor) {
 
@@ -26,10 +32,87 @@ public class JsonConfigPlugin implements GoPlugin {
 
     @Override
     public GoPluginApiResponse handle(GoPluginApiRequest request) throws UnhandledRequestTypeException {
+        String requestName = request.requestName();
+        if (requestName.equals(PLUGIN_SETTINGS_GET_CONFIGURATION)) {
+            return handleGetPluginSettingsConfiguration();
+        } else if (requestName.equals(PLUGIN_SETTINGS_GET_VIEW)) {
+            try {
+                return handleGetPluginSettingsView();
+            } catch (IOException e) {
+                return renderJSON(500, String.format("Failed to find template: %s", e.getMessage()));
+            }
+        } else if (requestName.equals(PLUGIN_SETTINGS_VALIDATE_CONFIGURATION)) {
+            return handleValidatePluginSettingsConfiguration(request);
+        }
         if ("parse-directory".equals(request.requestName())) {
             return handleParseDirectoryRequest(request);
         }
         throw new UnhandledRequestTypeException(request.requestName());
+    }
+    private GoPluginApiResponse handleGetPluginSettingsView() throws IOException {
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put("template", IOUtils.toString(getClass().getResourceAsStream("/plugin-settings.template.html"), "UTF-8"));
+        return renderJSON(DefaultGoPluginApiResponse.SUCCESS_RESPONSE_CODE, response);
+    }
+    private GoPluginApiResponse handleValidatePluginSettingsConfiguration(GoPluginApiRequest goPluginApiRequest) {
+        Map<String, Object> responseMap = (Map<String, Object>) JSONUtils.fromJSON(goPluginApiRequest.requestBody());
+        final Map<String, String> configuration = keyValuePairs(responseMap, "plugin-settings");
+        List<Map<String, Object>> response = new ArrayList<Map<String, Object>>();
+
+        //TODO actual validation
+        validate(response);
+
+        return renderJSON(DefaultGoPluginApiResponse.SUCCESS_RESPONSE_CODE, response);
+    }
+    private void validate(List<Map<String, Object>> response) {
+        Map<String, Object> fieldValidation = new HashMap<String, Object>();
+        if (!fieldValidation.isEmpty()) {
+            response.add(fieldValidation);
+        }
+    }
+    private Map<String, String> keyValuePairs(Map<String, Object> map, String mainKey) {
+        Map<String, String> keyValuePairs = new HashMap<String, String>();
+        Map<String, Object> fieldsMap = (Map<String, Object>) map.get(mainKey);
+        for (String field : fieldsMap.keySet()) {
+            Map<String, Object> fieldProperties = (Map<String, Object>) fieldsMap.get(field);
+            String value = (String) fieldProperties.get("value");
+            keyValuePairs.put(field, value);
+        }
+        return keyValuePairs;
+    }
+
+    private GoPluginApiResponse handleGetPluginSettingsConfiguration() {
+        Map<String, Object> response = new HashMap<String, Object>();
+        response.put(PLUGIN_SETTINGS_ENVIRONMENT_PATTERN, createField("Environment pattern", "**/*.goenvironment.json", false, false, "0"));
+        return renderJSON(DefaultGoPluginApiResponse.SUCCESS_RESPONSE_CODE, response);
+    }
+    private Map<String, Object> createField(String displayName, String defaultValue, boolean isRequired, boolean isSecure, String displayOrder) {
+        Map<String, Object> fieldProperties = new HashMap<String, Object>();
+        fieldProperties.put("display-name", displayName);
+        fieldProperties.put("default-value", defaultValue);
+        fieldProperties.put("required", isRequired);
+        fieldProperties.put("secure", isSecure);
+        fieldProperties.put("display-order", displayOrder);
+        return fieldProperties;
+    }
+    private GoPluginApiResponse renderJSON(final int responseCode, Object response) {
+        final String json = response == null ? null : new GsonBuilder().create().toJson(response);
+        return new GoPluginApiResponse() {
+            @Override
+            public int responseCode() {
+                return responseCode;
+            }
+
+            @Override
+            public Map<String, String> responseHeaders() {
+                return null;
+            }
+
+            @Override
+            public String responseBody() {
+                return json;
+            }
+        };
     }
 
     private GoPluginApiResponse handleParseDirectoryRequest(GoPluginApiRequest request) {
