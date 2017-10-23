2d71dc19d524dce7933258ec9b2bb6a096b7fa13
Aravind SV <arvind.sv@gmail.com>
2017-10-23 15:46:04 -0400

    Accept multiple patterns

--- src/com.tw.go.config.json/AntDirectoryScanner.java
+++ src/com.tw.go.config.json/AntDirectoryScanner.java
@@ -5,17 +5,11 @@ import org.apache.tools.ant.DirectoryScanner;
 import java.io.File;
 
 public class AntDirectoryScanner implements ConfigDirectoryScanner {
-
-    public AntDirectoryScanner()
-    {
-
-    }
-
     @Override
     public String[] getFilesMatchingPattern(File directory, String pattern) {
         DirectoryScanner scanner = new DirectoryScanner();
         scanner.setBasedir(directory);
-        scanner.setIncludes(new String[]{pattern});
+        scanner.setIncludes(pattern.trim().split(" *, *"));
         scanner.scan();
         return scanner.getIncludedFiles();
     }
